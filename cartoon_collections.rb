a8a0506791ba09710d07632acd75340a353d9dac
Renata Freitas <r.miriuk@gmail.com>
2019-11-18 15:48:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  myhash = Hash.new
+  array.each_with_index { |item,index|
+    myhash[index + 1] = item
+  }
+  puts myhash
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(names)# code an argument here
   # Your code here
+  names.map {|name| p "#{name.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.any? { |e| e.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.compare(cheese_types)
+
 end
+
+# .find(item => {
+#   return item.isAstronaut
+# })
